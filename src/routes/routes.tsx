b3b6c9a6d78be7f940520c97e75ee4d31939a460
Victor Pires <victorpiressk@gmail.com>
2026-01-14 21:27:58 -0300

    feat: atualização das rotas para suportar layouts aninhados
    - Implementação de nested routes com MainLayout
    - Separa rotas públicas (Login) de rotas privadas (Home)
    - Add rota /login
    - Usa Outlet para renderizar rotas filhas

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,21 @@
 import { Routes, Route } from 'react-router-dom'
+import MainLayout from '../components/Layout/MainLayout'
 import Home from '../pages/Home'
+import Login from '../pages/Login'
 
-// Componente de rotas principais
-const AppRoutes = () => (
-  <Routes>
-    <Route path="/home" element={<Home />} />
-  </Routes>
-)
+const AppRoutes = () => {
+  return (
+    <Routes>
+      {/* Rotas SEM Sidebar */}
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+
+      {/* Rotas COM Sidebar (usa MainLayout) */}
+      <Route element={<MainLayout />}>
+        <Route path="/home" element={<Home />} />
+      </Route>
+    </Routes>
+  )
+}
 
 export default AppRoutes
